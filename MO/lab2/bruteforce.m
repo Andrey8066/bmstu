65c3ef3bb66ba1a9790241630cd8c6285e4c76f0
andrsoin2089 <andrsoin@yandex.ru>
2026-04-04 14:55:18 +0300

    Написана математическая часть ДЗ по электронике

---
+++ MO/lab2/bruteforce.m
@@ -0,0 +1,33 @@
+
+function [x_min, y_min, k] = bruteforce(func, x_left, x_right, e)
+    
+    f_last = func(x_left);
+    a = -0.1;
+    delta = 0.1;
+    x = x_left;
+    k = 0;
+    
+    while x_left < x < x_right
+
+        k= k + 1;
+        f = func(x);
+
+        if f < f_last
+            x_min = x;
+
+        else
+            delta = a * delta;
+        end
+
+        f_last = f;
+        x = x + delta;
+
+        if e > abs(delta)
+            break
+        end
+        
+    end
+
+    y_min = func(x_min);
+
+end
--- MO/lab2/coordinateDowngrade.m
+++
@@ -1,9 +0,0 @@
-function [x, y, z, k] = coordinateDowngrade(func, X0, e )
-
-E = eye(length(x));
-while abs(Xn - X0) > e
-    
-    L = min(func(X + L * E));
-
-end
-end
